bfd59a0c5c59e6db9bed0c55fbc98bf13add4f92
Sam Urmy <urmy@mbari.org>
2020-01-09 16:57:29 -0800

    Make new start_datetime and stop_datetime columns instead of altering start and stop type

--- src/io.jl
+++ src/io.jl
@@ -5,17 +5,20 @@
     as_dataframe(clicks::Vector{T}) where {T<:AbstractClick}
 
 Converts a vector of `ClickPointer` or `Click` structures to tabular form as a
-`DataFrame`. If optional argument `starttime` is provided, the `DataFrame`
-the clicks' start and end offsets will be converted to `DateTime`s relative
-to `starttime` (i.e., the time of the first sample in the analyzed file).  The
-source file for the clicks can be included via th optional argument `wavfile`
-(either a `String` or `Vector{String}`).
+`DataFrame`. If optional argument `starttime` is provided, the returned
+`DataFrame` will also include `start_datetime` and `stop_datetime` columns,
+with the clicks' start and end times relative to the start of the sound file
+converted to `DateTime`s relative to `starttime` (i.e., the time of the first
+sample in the analyzed file).  The source file for the clicks can be included
+via th optional argument `wavfile` (either a `String` or `Vector{String}`).
 """
 function as_dataframe(clicks::Vector{T}, starttime::DateTime) where {T<:AbstractClick}
     df = DataFrame(
-        (start=starttime + Dates.Millisecond(round(left(c) * 1e3)),
-        stop=starttime + Dates.Millisecond(round(right(c) * 1e3)),
-        waveform=samples(c)) for c in clicks)
+        (start=left(c),
+         stop=right(c),
+         start_datetime=starttime + Dates.Millisecond(round(left(c) * 1e3)),
+         stop_datetime=starttime + Dates.Millisecond(round(right(c) * 1e3)),
+         waveform=samples(c)) for c in clicks)
     return df
 end
 
